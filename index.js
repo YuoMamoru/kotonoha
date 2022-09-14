c7513398b185b386c5e0f517ff3e42ad5af87a69
YuoMamoru <myuo@cam.hi-ho.ne.jp>
2022-09-14 11:14:23 +0900

    :+1: Easier handling

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ import * as kotonoha from './lib/kotonoha.js';
 
 // minMaxCount('./dictionary/kotonoha.txt', './dictionary/count.txt');
 
-kotonoha.outputCandidates('./dictionary/kotonoha.txt', './work/1.txt', (word) => (
-  word[0] !== 'ウ' && word.includes('ウ') &&
-  word.match(/^[^インリー]*$/)
+const step = 1
+
+function dict(i) {
+  return i ? `./work/${i}.txt` : './dictionary/kotonoha.txt';
+}
+
+kotonoha.outputCandidates(dict(step - 1), dict(step), (word) => (
+  // word[0] !== 'ウ' && word.includes('ウ') &&
+  // word[1] !== 'イ' && word.includes('イ') &&
+  // word[2] !== 'ン' && word.includes('ン') &&
+  // word[3] !== 'リ' && word.includes('リ') &&
+  // word[4] !== 'ー' && word.includes('ー') &&
+  word.match(/^[^ウインリー]*$/)
 ));
 
-kotonoha.minMaxCount('./work/1.txt', './work/count1.txt', 'ウ');
-kotonoha.countCharPosition('./work/1.txt', 'ウ');
+kotonoha.minMaxCount(dict(step), `./work/count${step}.txt`, 'ウ');
+kotonoha.countCharPosition(dict(step), 'ウ');
