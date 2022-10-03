a20f43c8f69ffc51f5038ad70fd3f1f049038015
YuoMamoru <myuo@cam.hi-ho.ne.jp>
2022-10-04 01:24:53 +0900

    :+1: Easier regexp

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ function dict(i) {
   return i ? `./work/${i}.txt` : './dictionary/kotonoha.txt';
 }
 
+const re = '[^ウインリー]';
 kotonoha.outputCandidates(dict(step - 1), dict(step), (word) => (
   // word[0] !== 'ウ' && word.includes('ウ') &&
   // word[1] !== 'イ' && word.includes('イ') &&
   // word[2] !== 'ン' && word.includes('ン') &&
   // word[3] !== 'リ' && word.includes('リ') &&
   // word[4] !== 'ー' && word.includes('ー') &&
-  word.match(/^[^ウインリー]*$/)
+  word.match(new RegExp(`^${re}*$`))
 ));
 
 kotonoha.minMaxCount(dict(step), `./work/count${step}.txt`, 'ウ');
